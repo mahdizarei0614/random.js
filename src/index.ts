b33ebae5b36bfa5f61535acf584d89f950006575
skyBlue <mahdizarei0614@gmail.com>
2024-03-29 01:11:55 +0330

    bugfix on overriding configs

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {RandomImage} from "./image/image";
 import {RandomNumber} from "./number/number";
 import {RandomBoolean} from "./boolean/boolean";
 import {RandomDate} from "./date/date";
-import {Config, PartialConfig} from "./utils/config.model";
+import {PartialConfig} from "./utils/config.model";
 import {config as defaultConfig} from "./base/default.config"
 
 export class Random {
@@ -14,12 +14,11 @@ export class Random {
     date;
 
     constructor(userConfig: PartialConfig = {}) {
-        const config = {...defaultConfig, ...userConfig} as Config;
-        this.string = new RandomString(config.string);
+        this.string = new RandomString({...defaultConfig.string, ...userConfig.string});
         this.image = new RandomImage();
-        this.number = new RandomNumber(config.number);
-        this.boolean = new RandomBoolean(config.boolean);
-        this.date = new RandomDate(config.date);
+        this.number = new RandomNumber({...defaultConfig.number, ...userConfig.number});
+        this.boolean = new RandomBoolean({...defaultConfig.boolean, ...userConfig.boolean});
+        this.date = new RandomDate({...defaultConfig.date, ...userConfig.date});
     }
 }
 
